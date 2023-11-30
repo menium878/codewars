f93fb6129a7c0f73e098908576e766cfd3490b6c
unknown <meus13@gmail.com>
2023-11-30 22:12:54 +0100

    nbYear

---
+++ nbYear.cpp
@@ -0,0 +1,16 @@
+class Arge
+{
+public:
+    static int nbYear(int p0, double percent, int aug, int p);
+
+};
+
+int Arge::nbYear(int p0, double percent, int aug, int p) {
+    int count=0;
+    while(p0<p){
+        p0+= p0 * (percent / 100) + aug;
+        count++;
+    }
+
+    return count;
+}
