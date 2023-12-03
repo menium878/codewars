a57d9b3080eb310c7acf35ee84693e0a195775f4
unknown <meus13@gmail.com>
2023-12-03 23:20:50 +0100

    duplicate_encoder

---
+++ duplicate_encoder.cpp
@@ -0,0 +1,13 @@
+std::string duplicate_encoder(const std::string& word){
+    std::string words;
+    std::string woord=word;
+    std::transform(woord.begin(), woord.end(), woord.begin(), ::tolower);
+    for (int i = 0; i < woord.length(); i++) {
+        if ((woord.find_first_of(woord[i]) != i) || (woord.find_last_of(woord[i]) != i)) {
+            words.push_back(')');
+        } else{
+            words.push_back('(');
+        }
+    }
+    return words;
+}
