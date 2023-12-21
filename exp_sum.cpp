deea2daa061784f7f6c2fa255c8bd7233f85c74e
unknown <meus13@gmail.com>
2023-12-21 14:43:40 +0100

    exp_sum

--- exp_sum.cpp
+++ exp_sum.cpp
@@ -1,3 +1,13 @@
-//
-// Created by Menium on 12/21/2023.
-//
+using ull = unsigned long long;
+
+ull exp_sum(unsigned int n) {
+
+    std::vector<ull> many(n+1, 0);
+    many[0] = 1;
+    for(unsigned int i = 1; i <= n; i++){
+        for(unsigned int j = i; j <= n; j++){
+            many[j] += many[j - i];
+        }
+    }
+    return many[n];
+}
