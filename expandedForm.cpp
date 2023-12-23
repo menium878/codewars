4a2d7d11b4f33574c991d2d3d53e514a7bf6c0af
unknown <meus13@gmail.com>
2023-12-23 20:52:04 +0100

    expandedForm

--- expandedForm.cpp
+++ expandedForm.cpp
@@ -1,3 +1,26 @@
-//
-// Created by Menium on 12/23/2023.
-//
+#include <string>
+
+std::string expandedForm(unsigned long long n) {
+    std::string s=std::to_string(n);
+    std::reverse(s.begin(), s.end());
+    int i=0;
+    std::string final;
+    for(char ha:s){
+        if (ha=='0') {
+            i++;
+            continue;
+        }
+        for(int k=0 ;k<i;k++){
+            final.insert(final.begin(),'0');
+        }
+        final.insert(final.begin(),ha);
+
+        if(s.length()-1!=i) {
+            final.insert(final.begin(),' ');
+            final.insert(final.begin(),'+');
+            final.insert(final.begin(),' ');
+        }
+        i++;
+    }
+    return final;
+}
