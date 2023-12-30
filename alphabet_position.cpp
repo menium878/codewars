2a838ab2de0dd408dec03dcca25c304a5432f4d9
unknown <meus13@gmail.com>
2023-12-30 04:48:23 +0100

    alphabet_position

--- alphabet_position.cpp
+++ alphabet_position.cpp
@@ -1,3 +1,13 @@
-//
-// Created by Menium on 12/30/2023.
-//
+#include <string>
+
+std::string alphabet_position(const std::string &text) {
+    std::string k;
+    for (auto ha : text){
+        if(tolower(ha)>96 && tolower(ha)<122) {
+            k+=std::to_string((tolower(ha) - 96));
+            k+=" ";
+        }
+    }
+    if(k.length()>0) k.pop_back();
+    return k;
+}
