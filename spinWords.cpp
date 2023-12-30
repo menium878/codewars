c964748982da4696b443931f91403c37065db813
unknown <meus13@gmail.com>
2023-12-30 06:38:54 +0100

    spinWords solved

--- spinWords.cpp
+++ spinWords.cpp
@@ -1,3 +1,15 @@
-//
-// Created by Menium on 12/30/2023.
-//
+#include <sstream>
+#include <string>
+std::string spinWords(const std::string &str)
+{
+    std::string result;
+    std::string ha=str;
+    std::stringstream ss(ha);
+    while(std::getline(ss,ha,' ')){
+        if(ha.length()>4) std::reverse(ha.begin(), ha.end());
+        result+=ha;
+        result+=" ";
+    }
+    if(!result.empty())result.pop_back();
+    return result;
+}
