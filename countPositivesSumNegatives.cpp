0d6dc6cb7f5274fd9bf75b32ffcb98d2e111607f
unknown <meus13@gmail.com>
2023-12-31 19:09:05 +0100

    countPositivesSumNegatives solution

--- countPositivesSumNegatives.cpp
+++ countPositivesSumNegatives.cpp
@@ -1,3 +1,12 @@
-//
-// Created by Menium on 12/31/2023.
-//
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+std::vector<int> countPositivesSumNegatives(std::vector<int> input)
+{
+    std::vector<int> k;
+    if(input.empty()) return k;
+    k.push_back(std::count_if(input.begin(),input.end(),[](auto a){return a>0;}));
+    k.push_back(std::accumulate(input.begin(),input.end(),0,[](auto a,auto b){return b<0?a+b:a;}));
+    return k;
+}
