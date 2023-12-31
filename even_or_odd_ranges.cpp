49b5556c3befa52e1f3ed3ff73dd5dc71305002a
unknown <meus13@gmail.com>
2023-12-31 19:49:49 +0100

    even_or_odd_ranges

--- even_or_odd_ranges.cpp
+++ even_or_odd_ranges.cpp
@@ -1,3 +1,10 @@
-//
-// Created by Menium on 12/31/2023.
-//
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+std::vector<std::string> even_or_odd(std::vector<int> number)
+{
+    std::vector<std::string> a;
+    std::transform(number.begin(), number.end(),std::back_inserter(a),[](int b){return b%2==0?"Even":"Odd";});
+    return a;
+}
