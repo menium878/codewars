3912b83a2f078792d6eb8c0e330c57c20bf1c607
unknown <meus13@gmail.com>
2024-01-07 02:01:49 +0100

    claiming solved

--- extra_climing_stairs.cpp
+++ extra_climing_stairs.cpp
@@ -1,3 +1,6 @@
-//
-// Created by Menium on 1/7/2024.
-//
+int climing_stairs(int number)
+{
+    if(number ==1)return 1;
+    if(number ==2)return 2;
+    return climing_stairs(number-1)+climing_stairs(number-2);
+}
