48848ee4c35238421edb7669ddcf869630b69478
unknown <meus13@gmail.com>
2024-01-12 03:34:36 +0100

    getCount solved with lambda

--- getCount.cpp
+++ getCount.cpp
@@ -1,3 +1,7 @@
-//
-// Created by Menium on 1/12/2024.
-//
+#include <string>
+#include <algorithm>
+
+
+int getCount(const std::string& inputStr){
+    return std::count_if(inputStr.begin(), inputStr.end(),[](char a){return a=='a'||a=='e'||a=='i'||a=='o'||a=='u';});
+}
