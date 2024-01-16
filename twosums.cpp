8bae2a732a6d93dc17079abfe49ecf20adeb2f26
unknown <meus13@gmail.com>
2024-01-16 20:32:25 +0100

    solution 2

--- twosums.cpp
+++ twosums.cpp
@@ -1,10 +1,24 @@
 #include <vector>
-std::vector<int> twoSum(std::vector<int>& nums, int target) {
-    for(int i=0;i<nums.size();i++){
-        for(int j=1;j<nums.size();j++){
-            if (nums[i]+nums[j]==target)
-                return {nums[i],nums[j]};
+#include <unordered_map>
+std::vector<int> twoSum(std::vector<int> &nums, int target) {
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[i] + nums[j] == target)
+                return {nums[i], nums[j]};
         }
     }
-    return {0,0};
+    return {0, 0};
+}
+
+std::vector<int> twoSum(std::vector<int> &nums, int target) {
+
+    std::unordered_map<int, int> numMap;
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (numMap.count(complement)) {
+            return {numMap[complement], i};
+        }
+        numMap[nums[i]] = i;
+    }
+    return {0, 0};
 }
