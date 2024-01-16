e5f2b199f9586c8739a0b1008ed89cd77b6e94b1
unknown <meus13@gmail.com>
2024-01-16 20:32:21 +0100

    solution 1

--- twosums.cpp
+++ twosums.cpp
@@ -1,3 +1,10 @@
-//
-// Created by Menium on 1/16/2024.
-//
+#include <vector>
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    for(int i=0;i<nums.size();i++){
+        for(int j=1;j<nums.size();j++){
+            if (nums[i]+nums[j]==target)
+                return {nums[i],nums[j]};
+        }
+    }
+    return {0,0};
+}
