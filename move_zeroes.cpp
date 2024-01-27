63c2896375bea8ef8dfe388e471ad0149b86bc73
unknown <meus13@gmail.com>
2024-01-27 21:14:12 +0100

    move_zeroes solved

--- move_zeroes.cpp
+++ move_zeroes.cpp
@@ -1,3 +1,14 @@
-//
-// Created by Menium on 1/27/2024.
-//
+#include <vector>
+
+std::vector<int> move_zeroes(const std::vector<int>& input) {
+    std::vector<int> k;
+    k=input;
+    k.erase(std::remove_if(k.begin(), k.end(), [](const auto &item) {
+        return item==0;
+    }),k.end());
+    int s=input.size()-k.size();
+    for(int i=0;i<s;i++){
+        k.emplace_back(0);
+    }
+    return k;
+}
