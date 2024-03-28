06b522ed476502b76535e62bdbf37cd23c9e7f14
Menium <97258926+menium878@users.noreply.github.com>
2024-03-28 14:18:47 +0100

    Update Follow_that_Spy.cpp

--- Follow_that_Spy.cpp
+++ Follow_that_Spy.cpp
@@ -1,1 +1,26 @@
+#include <vector>
+#include <string>
+#include <utility>
 
+std::string find_routes (const std::vector<std::pair<std::string, std::string>> &routes) {
+    std::pair<std::string, std::string> starting;
+    for(auto k:routes){
+        auto c = std::find_if(routes.begin(),routes.end(),[k](std::pair<std::string, std::string> a){return a.second==k.first;});
+        if(c==routes.end()) {
+            starting=k;
+            break;
+        }
+    }
+    std::string ret=starting.first+", ";
+    auto start=starting.second;
+
+    for(;;){
+        ret+=start;
+        auto k = std::find_if(routes.begin(),routes.end(),[&start](std::pair<std::string, std::string> a){return a.first==start;});
+        if(k==routes.end()) break;
+        auto h= *k;
+        ret+=+", ";
+        start=h.second;
+    }
+    return ret;
+}
