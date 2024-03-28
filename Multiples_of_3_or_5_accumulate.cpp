ea7f92c09221102b4cc38cbdc9433d3287262774
Menium <97258926+menium878@users.noreply.github.com>
2024-03-28 11:51:54 +0100

    Update Multiples_of_3_or_5_accumulate.cpp

--- Multiples_of_3_or_5_accumulate.cpp
+++ Multiples_of_3_or_5_accumulate.cpp
@@ -1,1 +1,9 @@
-
+#include <numeric>
+#include <vector>
+int solution(int number) 
+{
+if (number<=0)return 0; 
+    std::vector<int> vec(number);
+    iota(vec.begin()+1, vec.end(), 1);
+    return std::accumulate(vec.begin(),vec.end(),0,[](int a,int b){return b%5==0||b%3==0?a+b:a;});
+}
