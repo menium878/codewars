e23389031f2f8ff28d740d51da1be8e9adb54d61
Menium <97258926+menium878@users.noreply.github.com>
2024-04-13 00:09:19 +0200

    Update quetime.cc

--- quetime.cc
+++ quetime.cc
@@ -14,3 +14,11 @@ while(!customers.empty()){
 }
 
 
+long queueTime(std::vector<int> customers,int n){
+  std::vector<long> queues(n, 0);
+  
+  for (int i : customers)
+    *std::min_element(queues.begin(), queues.end()) += i;
+    
+  return *std::max_element(queues.cbegin(), queues.cend());
+}
