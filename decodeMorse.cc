df04304263ba6f66dfeb646fdd206450bcb9c5c4
Menium <97258926+menium878@users.noreply.github.com>
2024-11-08 00:01:21 +0100

    Update decodeMorse.cc

--- decodeMorse.cc
+++ decodeMorse.cc
@@ -1,1 +1,26 @@
+#include <string>
 
+std::string decodeMorse(const std::string& morseCode) {
+    std::string decoded;
+    std::string k="";
+    bool czy = false;
+    for( auto& p : morseCode ) {
+      if(p!=' ') {
+        k+=p;
+        czy = true;
+        }
+      if (!czy) continue;
+      if(k=="" && p==' ') {
+        if(decoded.back()==' ') czy =false;
+        else{decoded+=" ";
+        k=" ";}
+        continue;
+        }
+      if(p==' ' || &p == &morseCode.back()){
+      decoded += MORSE_CODE[k];
+        k="";
+        }
+    }
+    if(decoded.back()==' ') decoded.pop_back();
+    return decoded;
+}
